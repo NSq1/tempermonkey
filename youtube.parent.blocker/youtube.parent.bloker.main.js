ea17d1f19bbd6e58970d855f36a7995c392cab00
NSq1 <michal.wiewiorowski@gmail.com>
2020-12-27 10:51:45 +0100

    [YBL] current progress - init

---
+++ youtube.parent.blocker/youtube.parent.bloker.main.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Youtube Parent Blocker
+// @namespace    https://bitbucket.org/NSq1/
+// @version      0.8.0
+// @description  Blocks youtube and requires someone else to unlock any video
+// @author       NSq1
+// @match        https://www.youtube.com/watch*
+// @match        https://www.youtube.com/channel*
+// @grant        none
+// ==/UserScript==
